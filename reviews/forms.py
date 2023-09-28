0b5a5b9aed52cd9f1ebe9a1f416048b778449285
Lalbahadur786 <guptalalbahadur@gmail.com>
2023-09-28 10:16:43 +0530

    Added more form control and loop for form rendering

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,3 +3,5 @@ from django import forms
 class ReviewForm(forms.Form):
     user_name = forms.CharField(label="Your Name", max_length=20,error_messages={"required": "You Name should not be empty",
                    "max_length": "Please provide shorter name"})
+    review = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
