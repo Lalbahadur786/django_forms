68e067d3fc4ba28691ed60171ed602af8844e2f9
Lalbahadur786 <guptalalbahadur@gmail.com>
2023-10-01 19:43:46 +0530

    Added file upload functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
+from .form import ProfileForm
+from .models import UserProfileModel
 # Create your views here.
+
+# def store_file(file):
+#     with open("temp\meena.jpg", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
     
     def post(self, request):
-        pass
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            data = UserProfileModel(user_profile=request.FILES["user_file"])
+            data.save()
+            # store_file(request.FILES["user_file"])
+            return HttpResponseRedirect("/profiles")
+        else:
+            return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
